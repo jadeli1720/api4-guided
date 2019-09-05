f3eba83e31247fe21572b8a61545a79771d90173
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-05 11:44:26 -0600

    Trying to fix error and make port dynamic

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();//must come before anything else
 const server = require('./api/server.js');
 const defaults = require('./config/defualt')
 
-const port = 4000;
+const port = process.env.PORT;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
