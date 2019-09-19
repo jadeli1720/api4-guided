84d31cbf121df7f89d41a9e358ec52a239725236
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-19 11:57:54 -0600

    postgress

--- data/db.js
+++ data/db.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 
-const environment = process.env.NODE_ENV || 'development';
+const environment = secrets.environment|| 'development';
 const config = require('../knexfile.js')[environment];
 
 //{ development: {} }
--- knexfile.js
+++ knexfile.js
@@ -19,16 +19,8 @@ module.exports = {
   },
 
   production: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: './data/shouts.db3',
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      },
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,//>>>>DATABASE_URL comes from Heroku. Used to connect to Heroku
     migrations: {
       directory: './data/migrations',
     },
@@ -37,3 +29,6 @@ module.exports = {
     },
   },
 };
+
+//connection string using amazon database for right now. May change in 10 minutes
+//postgres://yldqmjozrfukwp:aab0296dd3b296feb31d4209c801a83f794fa3522376d3d3fa85697a1143a1c9@ec2-107-20-167-241.compute-1.amazonaws.com:5432/dncfrlra4p6mn
